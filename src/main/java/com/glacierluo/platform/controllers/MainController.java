494e0ea08cb24ebf3dc9ba6ac91d429462628dcb
GlacierLuo <1090490148@qq.com>
2019-03-31 13:01:49 +0800

    使用easyexcel完成导出成excel的功能

--- src/main/java/com/glacierluo/platform/controllers/MainController.java
+++ src/main/java/com/glacierluo/platform/controllers/MainController.java
@@ -1,13 +1,24 @@
 package com.glacierluo.platform.controllers;
 
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.metadata.Sheet;
+import com.alibaba.excel.support.ExcelTypeEnum;
 import com.glacierluo.platform.entity.User;
 import com.glacierluo.platform.repository.UserRepository;
+import org.apache.commons.collections4.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.glacierluo.platform.classes.Json;
 
-import java.util.Optional;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 @RequestMapping(path="/")
@@ -40,6 +51,51 @@ public class MainController {
             return new Json("Update success!", 200);
         }else{
             return new Json("Data not complete", 400);
+        }
+    }
+
+//    @PostMapping("deleteUser")
+
+
+//    @GetMapping("userSheet")
+//    public void userSheet(HttpServletResponse response) throws IOException{
+//        List<User> users = new ArrayList<>();
+//        userRepository.findAll().forEach(users::add);
+//        ExcelU
+//    }
+
+    @GetMapping("/usersSheet")
+    public void userSheet(HttpServletRequest request, HttpServletResponse response){
+        ServletOutputStream out = null;
+        try {
+            out = response.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX, true);
+        String fileName = null;
+        fileName = new String(("UserInfo " + new SimpleDateFormat("yyyy-MM-dd").format(new Date()))
+                .getBytes(), StandardCharsets.UTF_8);
+        response.setContentType("multipart/form-data");
+        response.setCharacterEncoding("utf-8");
+        response.setHeader("Content-disposition", "attachment;filename="+fileName+".xlsx");
+        Sheet sheet1 = new Sheet(1, 0, User.class);
+        sheet1.setSheetName("sheet1");
+        List<List<String>> users = new ArrayList<>();
+//        userRepository.findAll().forEach(users::add);
+        for (User user : userRepository.findAll()) {
+//            List<String> temp = new ArrayList<>();
+//            temp.add(all.next().toListString());
+            users.add(user.toListString());
+        }
+        writer.write0(users, sheet1);
+        writer.finish();
+        try {
+            if (out != null) {
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
